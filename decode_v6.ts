5d2fd02c77640359c9fd964b13477f66da7f8e9f
Skye A <christiaanmens@gmail.com>
2024-11-04 13:10:36 +0100

    x

---
+++ decode_v6.ts
@@ -0,0 +1,63 @@
+const CONTINUE_BIT = 0b10000000;
+const VALUE_MASK = 0b01111111;
+
+export function jsDecodeV6(input: Uint8Array): number {
+    // Get all continue bits
+    const bContinueBit = input[0] & CONTINUE_BIT;
+    // Use previous continuebit to nullify if the varint is smaller than the buffer
+    // Eg: [0xFF, 0x07, 0x10] (varint is 2 bytes but buffer is 3)
+    const cContinueBit = input[1] & CONTINUE_BIT & bContinueBit;
+    const dContinueBit = input[2] & CONTINUE_BIT & cContinueBit;
+    const eContinueBit = input[3] & CONTINUE_BIT & dContinueBit;
+
+    // no complex mask needed. First value always get processed.
+    const aMask = VALUE_MASK;
+
+    // use the `continue mask - 1|0` to create `0b10000000|0b01111111`
+    // then AND that with `0b01111111`
+    const bMask = VALUE_MASK & CONTINUE_BIT - (bContinueBit >> 7);
+    const cMask = VALUE_MASK & CONTINUE_BIT - (cContinueBit >> 7);
+    const dMask = VALUE_MASK & CONTINUE_BIT - (dContinueBit >> 7);
+    const eMask = VALUE_MASK & CONTINUE_BIT - (eContinueBit >> 7);
+
+    // Take all value bits
+    const a = input[0] & aMask;
+    // Shift into place
+    const b = (input[1] & bMask) << 7;
+    const c = (input[2] & cMask) << 14;
+    const d = (input[3] & dMask) << 21;
+    const e = (input[4] & eMask) << 28;
+
+    return a | b | c | d | e;
+}
+
+export function jsDecodeV6Opt(input: Uint8Array): number {
+    // Get all continue bits
+    const bContinueBit = input[0] & CONTINUE_BIT;
+    // Use previous continuebit to nullify if the varint is smaller than the buffer
+    // Eg: [0xFF, 0x07, 0x10] (varint is 2 bytes but buffer is 3)
+    const cContinueBit = input[1] & CONTINUE_BIT & bContinueBit;
+    const dContinueBit = input[2] & CONTINUE_BIT & cContinueBit;
+    const eContinueBit = input[3] & CONTINUE_BIT & dContinueBit;
+
+    // no complex mask needed. First value always get processed.
+    const aMask = VALUE_MASK;
+
+    // use the `continue mask - 1|0` to create `0b10000000|0b01111111`
+    // then AND that with `0b01111111`
+    const bMask = VALUE_MASK & VALUE_MASK + (bContinueBit >> 7);
+    const cMask = VALUE_MASK & VALUE_MASK + (cContinueBit >> 7);
+    const dMask = VALUE_MASK & VALUE_MASK + (dContinueBit >> 7);
+    const eMask = VALUE_MASK & VALUE_MASK + (eContinueBit >> 7);
+
+
+    // Take all value bits
+    const a = input[0] & aMask;
+    // Shift into place
+    const b = (input[1] & bMask) << 7;
+    const c = (input[2] & cMask) << 14;
+    const d = (input[3] & dMask) << 21;
+    const e = (input[4] & eMask) << 28;
+
+    return a | b | c | d | e;
+}
--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export {
   decode as stdDecode,
   encode,
 } from "https://deno.land/std@0.201.0/encoding/varint.ts";
-export { read as wasmDecode } from "https://raw.githubusercontent.com/denosaurs/byte_type/main/types/leb128/_i64leb128.ts";
+// export { read as wasmDecode } from "https://raw.githubusercontent.com/denosaurs/byte_type/main/types/leb128/_i64leb128.ts";
